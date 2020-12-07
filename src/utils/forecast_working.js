ce3d8bf6babb75f9cba8ba866436d3f7e82bea19
reid harward <reid.harward@gmail.com>
2020-12-07 08:11:23 -0500

    Initial commit

---
+++ src/utils/forecast_working.js
@@ -0,0 +1,24 @@
+const request = require('request')
+
+const forecast = (latitude, longitude, callback) => {
+    const url = 'http://api.weatherstack.com/current?access_key=3f71edbbd5237151846a6aca674e087f&query=' + latitude + ',' + longitude+'&units=f'
+
+    request({ url: url, json: true }, (error, response) => {
+        if (error) {
+            callback('Unable to connect to weather service!', undefined)
+        } else if (response.body.error) {
+            callback('Unable to find location', undefined)
+        } else {
+            callback(undefined, 'It is currently '+response.body.current.temperature+
+                                                  ' in '+ response.body.location.name+', '+
+                                                   response.body.location.region)
+            
+            //.daily.data[0].summary)
+                //+ ' It is currently ' + response.body.currently.temperature + ' degrees out. There is a ' + response.body.currently.precipProbability + '% chance of rain.')
+        }
+    })
+}
+
+
+module.exports = forecast
+
